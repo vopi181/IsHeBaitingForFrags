a42478851a2f0b4c607f72f6d43d928cc917e012
= <dompace181@gmail.com>
2021-01-18 18:49:15 -0600

    first

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/vopi181/IsHeBaitingForFrags
+
+go 1.15
+
+require (
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/golang/geo v0.0.0-20210108004804-a63082ebfb66 // indirect
+	github.com/markus-wa/demoinfocs-golang/v2 v2.5.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,109 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	dem "github.com/markus-wa/demoinfocs-golang/v2/pkg/demoinfocs"
+	events "github.com/markus-wa/demoinfocs-golang/v2/pkg/demoinfocs/events"
+)
+
+type baitStruct struct {
+	name       string
+	totalBaits int
+}
+
+//ders id
+// 76561198128945703
+func isBaitingFile(fd string, id int64, topBaiters bool) {
+	f, err := os.Open(fd)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	p := dem.NewParser(f)
+	defer p.Close()
+	totalBaits := 0
+	pname := ""
+
+	var tb map[int64]*baitStruct
+	tb = make(map[int64]*baitStruct)
+
+	p.RegisterEventHandler(func(e events.MatchStart) {
+		fmt.Println("======PLAYERS======")
+		for _, ply := range p.GameState().Participants().All() {
+			if topBaiters {
+				tb[int64(ply.SteamID64)] = &baitStruct{name: ply.Name, totalBaits: 0}
+				fmt.Printf("[%v] %v\n", ply.SteamID64, tb[int64(ply.SteamID64)].name)
+			} else {
+				fmt.Printf("[%v] %v\n", ply.SteamID64, ply.Name)
+			}
+		}
+		fmt.Println("===================")
+	})
+
+	p.RegisterEventHandler(func(e events.Kill) {
+		if p.GameState().IsMatchStarted() {
+			// fmt.Printf("[round #%v] %v killed %v\n", p.GameState().TotalRoundsPlayed(), e.Killer.Name, e.Victim.Name)
+			if e.Victim.SteamID64 == uint64(id) || topBaiters {
+				pname = e.Victim.Name
+				// Calculate people alive on team
+				aliveteam := 0
+				for _, tm := range e.Victim.TeamState.Members() {
+					if tm.LastAlivePosition != tm.Position() {
+						aliveteam = aliveteam + 1
+
+					}
+				}
+				if aliveteam == 1 {
+					println("Potential Baiter: " + e.Victim.Name + " at " + fmt.Sprint(p.GameState().TotalRoundsPlayed()))
+					totalBaits = totalBaits + 1
+					if topBaiters {
+						tb[int64(e.Victim.SteamID64)].totalBaits = (tb[int64(e.Victim.SteamID64)].totalBaits) + 1
+					}
+
+				}
+
+			}
+		}
+	})
+
+	p.RegisterEventHandler(func(e events.AnnouncementWinPanelMatch) {
+		if !topBaiters {
+			roundsPlayed := float32(p.GameState().TotalRoundsPlayed())
+			fTotalBaits := float32(totalBaits)
+			fmt.Println("======BAIT CALC======")
+			fmt.Printf("Player: %v(%v) on %v\n", pname, id, p.Header().MapName)
+			fmt.Printf("Baited %v/%v = %v percent of rounds\n", totalBaits, p.GameState().TotalRoundsPlayed(), (fTotalBaits/roundsPlayed)*100)
+			fmt.Println("=====================")
+		} else {
+			fmt.Println("======BAIT CALC======")
+			for _, ply := range p.GameState().Participants().All() {
+				roundsPlayed := float32(p.GameState().TotalRoundsPlayed())
+				fTotalBaits := float32(tb[int64(ply.SteamID64)].totalBaits)
+				fmt.Printf("Player: %v(%v) ", ply.Name, ply.SteamID64)
+				fmt.Printf("Baited %v/%v = %v percent of rounds\n", tb[int64(ply.SteamID64)].totalBaits, p.GameState().TotalRoundsPlayed(), (fTotalBaits/roundsPlayed)*100)
+			}
+			fmt.Println("=====================")
+
+		}
+	})
+	err = p.ParseToEnd()
+	if err != nil {
+		panic(err)
+	}
+
+}
+
+func main() {
+	println("Is He Baiting For Frags? - The Age Old Question")
+	demoFile := flag.String("demofile", "test.dem", "The demo file to parse")
+	steamID := flag.Int64("steamId", 76561198128945703, "SteamId64 of player to Watch")
+	topBaiters := flag.Bool("topBaiters", false, "Analyze every player")
+	flag.Parse()
+
+	isBaitingFile(*demoFile, *steamID, *topBaiters)
+
+}
